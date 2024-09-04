2203bd78a86378644ed1be6a5e2d1585d102754e
Adham Marzouk <adhammarzouk200@gmail.com>
2024-09-04 19:49:17 +0300

    Security Groups Configurations

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -40,3 +40,40 @@ resource "aws_route_table_association" "servers_rt_association" {
   route_table_id = aws_route_table.servers_subnet_rt.id
 }
 
+resource "aws_security_group" "servers_sg" {
+  vpc_id = aws_vpc.bridge_stream_vpc.id
+  name = "servers_sg"
+  description = "The security group configuration for the EC2 servers"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "servers_sg"
+  }
+}
